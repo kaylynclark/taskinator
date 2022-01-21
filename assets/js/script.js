298f3676c97b473db1badfc257899c1604fe1331
Ladybug <clark.kaylyn97@gmail.com>
2022-01-21 12:23:22 -0800

    custom values and validtion for form

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,12 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
 
+    // check if input values are empty strings (did user leave boxes empty)
+    if(!taskNameInput || !taskTypeInput) {
+        alert("You need to fill ou the task form!");
+        return false;
+    }
+    formEl.reset();
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -16,14 +22,19 @@ var taskFormHandler = function(event) {
     createTaskEl(taskDataObj);
     }
 var createTaskEl = function(taskDataObj) {
+    // create list item
     var listItemEl =document.createElement("li");
     listItemEl.className = "task-item";
 
+    // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
+    // give it a class name
     taskInfoEl.className = "task-info";
+    // add HTML content to div
     taskInfoEl.innerHTML =  "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl);
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 formEl.addEventListener("submit", taskFormHandler);
